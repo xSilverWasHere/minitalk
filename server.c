6712b7a195011839ac96f52a890c65ed7f167af9
Silver <fireon42@gmail.com>
2025-11-10 09:59:53 +0000

    fixed norm

--- server.c
+++ server.c
@@ -12,45 +12,40 @@
 
 #include "minitalk.h"
 
-void handle_client_signal(int signum, siginfo_t *siginfo, void *context)
+void	handle_client_signal(int signum, siginfo_t *siginfo, void *context)
 {
-  static char current_char;
-  static int bit_index;
-  (void)context;
+	static char	current_char;
+	static int	bit_index;
 
-  current_char <<= 1;
-  current_char |= (signum == SIGUSR1);
-  bit_index++;
-
-  if (bit_index == 8)
-  {
-      if (current_char == '\0')
-          ft_printf("\n");
-      else
-          ft_printf("%c", current_char);
-      bit_index = 0;
-      current_char = 0;
-  }
-  if (kill(siginfo->si_pid, SIGUSR1) == -1)
-        exit(ft_printf("Error sending signal\\n"));
+	(void) context;
+	current_char <<= 1;
+	current_char |= (signum == SIGUSR1);
+	bit_index++;
+	if (bit_index == 8)
+	{
+		if (current_char == '\0')
+			ft_printf("\n");
+		else
+			ft_printf("%c", current_char);
+		bit_index = 0;
+		current_char = 0;
+	}
+	if (kill(siginfo->si_pid, SIGUSR1) == -1)
+		exit(ft_printf("Error sending signal\\n"));
 }
 
-int main(void)
+int	main(void)
 {
-    struct sigaction sa;
+	struct sigaction	sa;
 
-    ft_bzero(&sa, sizeof(struct sigaction));
-
-    sa.sa_sigaction = &handle_client_signal;
-    sa.sa_flags = SA_SIGINFO;
-
-    if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
-        exit(ft_printf("Error setting up signal handler\n"));
-
-    ft_printf("Server PID: %d\n", getpid());
-
-    while (1)
-        pause();
-
-    return (0);
+	ft_bzero(&sa, sizeof(struct sigaction));
+	sa.sa_sigaction = &handle_client_signal;
+	sa.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1
+		|| sigaction(SIGUSR2, &sa, NULL) == -1)
+		exit(ft_printf("Error setting up signal handler\n"));
+	ft_printf("Server PID: %d\n", getpid());
+	while (1)
+		pause();
+	return (0);
 }
